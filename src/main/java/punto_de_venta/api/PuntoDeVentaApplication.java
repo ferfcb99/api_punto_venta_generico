1b80baa69609907183217c929cebfc34b55f8208
ferfcb99 <fer_fcb99@outlook.com>
2023-01-09 20:12:23 -0600

    Se sube proyecto

---
+++ src/main/java/punto_de_venta/api/PuntoDeVentaApplication.java
@@ -0,0 +1,13 @@
+package punto_de_venta.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoDeVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntoDeVentaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/punto_de_venta/api/PuntoDeVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package punto_de_venta.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntoDeVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
