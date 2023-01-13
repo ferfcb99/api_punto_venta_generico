289936798d82582878fae202d2481bf7cff64263
Fernando Munguía Moreno <fer_fcb99@outlook.com>
2023-01-13 17:01:35 -0600

    Se agrega category service

---
+++ src/main/java/punto_de_venta/api/services/generic/CategoryService.java
@@ -0,0 +1,44 @@
+package punto_de_venta.api.services.generic;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import punto_de_venta.api.daos.generic.CategoryDao;
+import punto_de_venta.api.entities.generic.Category;
+import punto_de_venta.api.interfaces.generic.services.ICategoryService;
+
+
+@Service
+public class CategoryService implements ICategoryService<Category> {
+
+    @Autowired
+    CategoryDao categoryDao;
+
+    @Override
+    public List<Category> getAll() {
+        List<Category> categories = this.categoryDao.findAll();
+        return categories;
+    }
+
+    @Override
+    public Category getById(Long id) {
+        Category category = this.categoryDao.getById(id);
+        return category;
+    }
+
+    @Override
+    public Category createOrUpdate(Category entity) {
+        Category category = this.createOrUpdate(entity);
+        return category;
+    }
+
+    @Override
+    public Category deleteById(Long id) {
+        Category category = this.getById(id);
+        this.categoryDao.deleteById(id);
+        return category;
+    }
+    
+}
