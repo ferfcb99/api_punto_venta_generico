9451e9b1549d034c319b6d55c218fd2f04dce7cf
Fernando Munguía Moreno <fer_fcb99@outlook.com>
2023-01-13 11:20:33 -0600

    Se agregan interfaces de servicios y controladores, se agrega dependencia log4j para implementar logs de la aplicacion

---
+++ src/main/java/punto_de_venta/api/interfaces/generic/controllers/IProviderController.java
@@ -0,0 +1,19 @@
+package punto_de_venta.api.interfaces.generic.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import punto_de_venta.api.dto.Response;
+
+public interface IProviderController<T> {
+
+    public ResponseEntity<Response<List<T>>> getAll();
+
+    public ResponseEntity<Response<T>> getById(Long id);
+
+    public ResponseEntity<Response<T>> createOrUpdate(T entity);
+
+    public ResponseEntity<Response<T>> deleteById(Long id);
+
+}
